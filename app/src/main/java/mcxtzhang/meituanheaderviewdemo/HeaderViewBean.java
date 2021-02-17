35a52f637c6b140e016f00221f53d0777253d03b
E_wsq <E_wsq@msn.com>
2021-02-18 03:04:02 +0800

    init

---
+++ app/src/main/java/mcxtzhang/meituanheaderviewdemo/HeaderViewBean.java
@@ -0,0 +1,14 @@
+package mcxtzhang.meituanheaderviewdemo;
+
+/**
+ * Created by zhangxutong on 2016/2/15.
+ */
+public class HeaderViewBean {
+    public String name;
+    public int iconRes;
+
+    public HeaderViewBean(String name, int iconRes) {
+        this.name = name;
+        this.iconRes = iconRes;
+    }
+}
